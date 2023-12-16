c120b0e5eb1f84f7310e4c9acc6cd5efd6583819
vaibhavhirmukhe <hirmukhevaibhav15@gmail.com>
2023-12-16 20:56:25 +0530

    new commit

--- backend/app.js
+++ backend/app.js
@@ -28,9 +28,9 @@ app.use("/api/v1", payment);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
 // middleware for Errors
 app.use(errorMiddlerware);
